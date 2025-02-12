0aec4cfd4cab13f0ab05d56d170759b039bc623e
Nikusha <nikusha@Nikas-MacBook-Pro.local>
2025-02-12 22:52:35 +0400

    style: add some styling

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/", carRoutes);
 
 app.listen(process.env.PORT, () =>
